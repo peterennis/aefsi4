993c5b2fa12affcc6bd3070978ed7f620469e291
Jorge Vergara <javebratt@gmail.com>
2018-08-01 20:30:38 -0500

    Create song page

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { FirestoreService } from '../../services/data/firestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public createSongForm: FormGroup;
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public firestoreService: FirestoreService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createSongForm = this.formBuilder.group({
+      albumName: ['', Validators.required],
+      artistName: ['', Validators.required],
+      songDescription: ['', Validators.required],
+      songName: ['', Validators.required],
+    });
   }
 
+  ngOnInit() {}
+
+  async createSong() {
+    const loading = await this.loadingCtrl.create();
+
+    const albumName = this.createSongForm.value.albumName;
+    const artistName = this.createSongForm.value.artistName;
+    const songDescription = this.createSongForm.value.songDescription;
+    const songName = this.createSongForm.value.songName;
+
+    this.firestoreService
+      .createSong(albumName, artistName, songDescription, songName)
+      .then(
+        () => {
+          this.router.navigateByUrl('');
+        },
+        error => {
+          console.error(error);
+        }
+      );
+
+    return await loading.present();
+  }
 }
--- src/app/services/data/firestore.service.ts
+++ src/app/services/data/firestore.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Song } from '../../models/song.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirestoreService {
+  constructor(public firestore: AngularFirestore) {}
 
-  constructor() { }
+  createSong(
+    albumName: string,
+    artistName: string,
+    songDescription: string,
+    songName: string
+  ): Promise<void> {
+    const id = this.firestore.createId();
+
+    return this.firestore.doc(`songList/${id}`).set({
+      id,
+      albumName,
+      artistName,
+      songDescription,
+      songName,
+    });
+  }
 }
